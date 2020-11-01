d5d331f9b314538e7e1e827b479289f4efa7b744
Abdul Samad <Samk8778@yahoo.com>
2020-11-01 20:33:58 +0530

    :zap: Optimized images and code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 window.addEventListener('load', loading, false);
 window.addEventListener('scroll', scrolling, false);
 
 function loading() {
-  var main = document.getElementById("main");
-  var loader = document.getElementById("loader");
-  main.style.display = "block";
-  loader.style.display = "none";
+	var main = document.getElementById('main');
+	var loader = document.getElementById('loader');
+	main.style.display = 'block';
+	loader.style.display = 'none';
 }
 
 function scrolling() {
-  var scrollTopSize = window.pageYOffset || document.documentElement.scrollTop;
-  var nav = document.getElementById('nav');
-  if (scrollTopSize > 0) {
-    nav.style.backgroundColor = "#000";
-  }
-  if (scrollTopSize <= 0) {
-    nav.style.backgroundColor = "transparent";
-  }
+	var scrollTopSize = window.pageYOffset || document.documentElement.scrollTop;
+	var nav = document.getElementById('nav');
+	if (scrollTopSize > 0) {
+		nav.style.backgroundColor = '#000';
+	}
+	if (scrollTopSize <= 0) {
+		nav.style.backgroundColor = 'transparent';
+	}
 }
